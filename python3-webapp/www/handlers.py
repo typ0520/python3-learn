8bd4664e04cea040b95c2d5a6a23ca65971d85d9
typ0520 <yapeng.tong@baidao.comc>
2017-07-17 16:56:03 +0800

    day-07

--- python3-webapp/www/handlers.py
+++ python3-webapp/www/handlers.py
@@ -6,9 +6,11 @@ from coroweb import get, post
 
 from models import User, Comment, Blog, next_id
 
+
 @get('/')
 async def index(request):
-	users = await User.findAll()
-	return {
-		'users': users
-	}
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
--- python3-webapp/www/orm.py
+++ python3-webapp/www/orm.py
@@ -88,7 +88,7 @@
 # 	def __new__(cls, name, bases, attrs):
 # 		if name == 'Model':
 # 			return type.__new__(cls ,name, bases ,attrs)
-		
+
 # 		tableName = attrs.get('__table__',None) or name
 
 # 		logging.info('found model: %s (table,%s)' % (name,tableName))
@@ -118,7 +118,7 @@
 # 		attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
 # 		attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
 # 		return type.__new__(cls ,name, bases ,attrs)
-		
+
 # def create_args_string(num):
 #     L = []
 #     for n in range(num):
@@ -226,8 +226,10 @@ import asyncio, logging
 
 import aiomysql
 
+
 def log(sql, args=()):
     logging.info('SQL: %s' % sql)
+
 
 @asyncio.coroutine
 def create_pool(loop, **kw):
@@ -246,6 +248,7 @@ def create_pool(loop, **kw):
         loop=loop
     )
 
+
 @asyncio.coroutine
 def select(sql, args, size=None):
     log(sql, args)
@@ -260,6 +263,7 @@ def select(sql, args, size=None):
         yield from cur.close()
         logging.info('rows returned: %s' % len(rs))
         return rs
+
 
 @asyncio.coroutine
 def execute(sql, args, autocommit=True):
@@ -280,14 +284,15 @@ def execute(sql, args, autocommit=True):
             raise
         return affected
 
+
 def create_args_string(num):
     L = []
     for n in range(num):
         L.append('?')
     return ', '.join(L)
 
-class Field(object):
 
+class Field(object):
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
@@ -297,35 +302,35 @@ class Field(object):
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
-class StringField(Field):
 
+class StringField(Field):
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
 
-class BooleanField(Field):
 
+class BooleanField(Field):
     def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
 
-class IntegerField(Field):
 
+class IntegerField(Field):
     def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
 
-class FloatField(Field):
 
+class FloatField(Field):
     def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, 'real', primary_key, default)
 
-class TextField(Field):
 
+class TextField(Field):
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
 
-class ModelMetaclass(type):
 
+class ModelMetaclass(type):
     def __new__(cls, name, bases, attrs):
-        if name=='Model':
+        if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
         tableName = attrs.get('__table__', None) or name
         logging.info('found model: %s (table: %s)' % (name, tableName))
@@ -348,18 +353,20 @@ class ModelMetaclass(type):
         for k in mappings.keys():
             attrs.pop(k)
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
         attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primaryKey # 主键属性名
-        attrs['__fields__'] = fields # 除主键外的属性名
+        attrs['__primary_key__'] = primaryKey  # 主键属性名
+        attrs['__fields__'] = fields  # 除主键外的属性名
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
-        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
+        tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
+        tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
-class Model(dict, metaclass=ModelMetaclass):
 
+class Model(dict, metaclass=ModelMetaclass):
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
 
@@ -457,6 +464,3 @@ class Model(dict, metaclass=ModelMetaclass):
         rows = yield from execute(self.__delete__, args)
         if rows != 1:
             logging.warn('failed to remove by primary key: affected rows: %s' % rows)
-
-
-
